47d755fa21ead72663af21d817722341df961916
TR0205 <65379809+TR0205@users.noreply.github.com>
2022-07-14 22:52:30 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import axios from "axios";
+import { UserCard } from "./components/UserCard";
+import { User } from "./types/API/user";
+import "./styles.css";
+import { useState } from "react";
+import { UserProfile } from "./types/userProfile";
+
+export default function App() {
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const onClickFetchUser = () => {
+    setLoading(true);
+    setError(false);
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.city}`
+        }));
+        setUserProfiles(data);
+      })
+      .catch(() => {
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+  return (
+    <div className="App">
+      <button onClick={onClickFetchUser}>データ取得</button>
+      <br />
+      {error ? (
+        <p>データの取得に失敗しました</p>
+      ) : loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {userProfiles.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </>
+      )}
+    </div>
+  );
+}
